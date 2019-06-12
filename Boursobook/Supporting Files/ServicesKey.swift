b1ea4a3b768da1fcd2c0ceca7a0a04daadaaaca6
ddubez <ddubez40@gmail.com>
2019-06-12 17:55:40 +0200

    add ServicesKey in gitignore

---
+++ Boursobook/Supporting Files/ServicesKey.swift
@@ -0,0 +1,15 @@
+//
+//  ServicesKey.swift
+//  Boursobook
+//
+//  Created by David Dubez on 12/06/2019.
+//  Copyright © 2019 David Dubez. All rights reserved.
+//
+
+import Foundation
+
+struct ServicesKey {
+    // Storage of private key
+    
+    static let apiKey = "nothing"
+}
